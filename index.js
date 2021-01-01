d1c566e98b3e9ca0184dd9ed0afdd816a76b6983
TheHubertos <67113860+TheHubertos@users.noreply.github.com>
2021-01-01 23:02:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,11 +76,11 @@ function sendcoms(chnl) {
 client.on('ready', () => {
     console.log(community_name + "'s Update Player BOT has been loaded.");
     console.log("Made By Noam#2111 | Aizik#5555");
-    bot.user.setStatus('dnd')
-    bot.user.setPresence({
+    client.user.setPresence({
         game: {
-            name: 'Użyj !pomoc',
-            type: "PLAYING"
+            name: 'Use f!help',
+            type: "Playing",
+            url: "https://discordapp.com/"
         }
     });
 });
